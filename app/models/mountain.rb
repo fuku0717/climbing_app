a6e59b0751557109cc8180217cf96a601950ddd8
fuku0717 <zhenchengs314@gmail.com>
2023-05-15 23:38:39 +0900

    バリデーションを設定

--- app/models/mountain.rb
+++ app/models/mountain.rb
@@ -1,4 +1,9 @@
 class Mountain < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  validates :mountain_name, presence: true
+  validates :level, presence: true
+  validates :point, presence: true
+  validates :image, presence: true
 end

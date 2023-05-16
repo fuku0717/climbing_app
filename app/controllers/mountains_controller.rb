e23983cd751b243f5035004cf3649c5fc73f54be
fuku0717 <zhenchengs314@gmail.com>
2023-05-16 19:24:55 +0900

    アクションとルーティングの設定

--- app/controllers/mountains_controller.rb
+++ app/controllers/mountains_controller.rb
@@ -1,4 +1,12 @@
 class MountainsController < ApplicationController
+
   def index
   end
+
+  def new
+  end
+
+  def create
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   get 'mountains/index'
   root to: "mountains#index"
+  resources :mountains, only: [:index, :new, :create]
 end

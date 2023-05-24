f1ceeafa8319b49b6593f2fc03b45a10a4cc11d0
fuku0717 <zhenchengs314@gmail.com>
2023-05-24 19:08:31 +0900

    commentsコントローラーの編集

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,12 @@
 class CommentsController < ApplicationController
   def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to mountain_path(@comment.mountain)
+    else
+      @mountain = @comment.mountain
+      @comments = @mountain.comments
+      render "mountains/show"
+    end
   end
 end

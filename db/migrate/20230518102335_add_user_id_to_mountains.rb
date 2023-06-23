ad5de7e06df2fd22a18192a760ca9f8c91a496dc
fuku0717 <zhenchengs314@gmail.com>
2023-06-23 12:54:42 +0900

    マイグレーションファイルの削除

--- db/migrate/20230518102335_add_user_id_to_mountains.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToMountains < ActiveRecord::Migration[6.0]
-  def change
-    add_column :mountains, :user_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_23_060656) do
+ActiveRecord::Schema.define(version: 2023_05_15_135940) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -33,23 +33,12 @@ ActiveRecord::Schema.define(version: 2023_05_23_060656) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.text "content", null: false
-    t.bigint "user_id", null: false
-    t.bigint "mountain_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["mountain_id"], name: "index_comments_on_mountain_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
-  end
-
   create_table "mountains", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "mountain_name"
     t.text "level"
     t.text "point"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -69,6 +58,4 @@ ActiveRecord::Schema.define(version: 2023_05_23_060656) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "comments", "mountains"
-  add_foreign_key "comments", "users"
 end

4dc2c58aca0e48b15efa271ac7f0ac5281bc8d1c
fuku0717 <zhenchengs314@gmail.com>
2023-06-27 15:49:39 +0900

    インスタンス変数を修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     if @comment.save
-      redirect_to mountain_path(@comments.mountain)
+      redirect_to mountain_path(@comment.mountain)
     else
       @mountain = @comment.mountain
       @comments = @mountain.comments

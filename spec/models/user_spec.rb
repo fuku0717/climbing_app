3228dce39932baf02a40d699abe233a1f5db9a69
fuku0717 <zhenchengs314@gmail.com>
2023-07-27 11:42:44 +0900

    メールアドレスが空では登録できない場合のテストコードを記述

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,11 +2,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'ユーザー新規登録' do
-    it 'nicknameが空では登録できない' do
-      # nicknameが空では登録できないテストコードを記述します
-    end
-    it 'emailが空では登録できない' do
-      # emailが空では登録できないテストコードを記述します
+    it 'メールアドレスが空では登録できない' do
+      user = User.new(email: '', password: '000000', password_confirmation: '000000', name: '太郎', profile:'ベテラン登山家', prefecture: '熊本県',history: '10年')
+      user.valid?
+      expect(user.errors.full_messages).to include("Email can't be blank")
     end
   end
 end

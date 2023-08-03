d2eb644158859720f3707a57a6316f90ccc86425
fuku0717 <zhenchengs314@gmail.com>
2023-08-03 20:36:17 +0900

    imageカラムの削除

--- spec/factories/mountains.rb
+++ spec/factories/mountains.rb
@@ -3,7 +3,6 @@ FactoryBot.define do
     :mountain_name { Faker::Lorem.sentence }
     :level         { Faker::Lorem.sentence }
     :point         { Faker::Lorem.sentence }
-    :image
 
     after(:build) do |mountain|
       mountain.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')

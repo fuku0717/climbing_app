83b0c4cb7dab9483f18f12b6e695215bbbb5806b
fuku0717 <zhenchengs314@gmail.com>
2023-08-03 20:31:44 +0900

    ｛　｝の中の空白を揃えた

--- spec/factories/mountains.rb
+++ spec/factories/mountains.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :mountain do
-    :mountain_name {Faker::Team.name}
+    :mountain_name { Faker::Team.name }
     :level         { Faker::Lorem.sentence }
     :point         { Faker::Lorem.sentence }
     :image

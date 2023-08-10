b7555b8760624baae02576f93b7fdf8acab45af5
fuku0717 <zhenchengs314@gmail.com>
2023-08-10 15:18:07 +0900

    syntax error解決

--- spec/factories/mountains.rb
+++ spec/factories/mountains.rb
@@ -1,8 +1,7 @@
 FactoryBot.define do
   factory :mountain do
     
-    :level         { Faker::Lorem.sentence }
-    :point         { Faker::Lorem.sentence }
+    
 
     after(:build) do |mountain|
       mountain.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')

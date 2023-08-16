607b0d27a73d32ede416e59415f050a09bf55fb1
fuku0717 <zhenchengs314@gmail.com>
2023-08-16 18:31:48 +0900

    application_controllerにaccount_updateの記述を加え、欲しい値を受け取れるようにする

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile, :prefecture, :history])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile, :prefecture, :history])
   end
 
   def basic_auth

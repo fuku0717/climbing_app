ab7bac300eab9d32084593996171b7eb214dcb3f
fuku0717 <zhenchengs314@gmail.com>
2023-08-16 17:48:19 +0900

    registrations_controller.rbを使えるようにルーティングを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "users/registrations"
+  }
   root to: "mountains#index"
   resources :mountains do
     resources :comments, only: :create

b25e22673fcb372ce7ea42b51d1cb23833c95407
fuku0717 <zhenchengs314@gmail.com>
2023-08-23 12:32:35 +0900

    usersコントローラーにeditアクションとupdateアクションの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,16 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
 end
